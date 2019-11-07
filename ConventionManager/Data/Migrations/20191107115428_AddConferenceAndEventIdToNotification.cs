477d6d26d2baa56692055202b484d5635677e74c
dasolari <dasolari@uc.cl>
2019-11-07 09:05:48 -0300

    Fixed repetition in notifications

---
+++ ConventionManager/Data/Migrations/20191107115428_AddConferenceAndEventIdToNotification.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConventionManager.Data.Migrations
+{
+    public partial class AddConferenceAndEventIdToNotification : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ConferenceId",
+                table: "Notifications",
+                nullable: true,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EventId",
+                table: "Notifications",
+                nullable: true,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConferenceId",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "EventId",
+                table: "Notifications");
+        }
+    }
+}
