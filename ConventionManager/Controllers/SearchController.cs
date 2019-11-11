305060af245e210b42e75b33d18422fd1e43ec31
dasolari <dasolari@uc.cl>
2019-11-10 23:24:30 -0300

    Finished user profile

--- ConventionManager/Controllers/SearchController.cs
+++ ConventionManager/Controllers/SearchController.cs
@@ -26,16 +26,17 @@ namespace ConventionManager.Controllers
         [HttpGet]
         public IActionResult GlobalSearch()
         {
-            return RedirectToAction("Index", "EventCenter");
+            var searchResults = new SearchResults();
+            return View(searchResults);
         }
 
         [HttpPost]
         public async Task<IActionResult> GlobalSearch(string searchString)
         {
-            var eventCenters = _context.EventCenters.Where(ev => ev.Name == searchString).ToArray();
-            var conferences = _context.Conferences.Where(c => c.Name == searchString).ToArray();
-            var events = _context.Events.Where(e => e.Name == searchString).ToArray();
-            var rooms = _context.Rooms.Where(r => r.Name == searchString).ToArray();
+            var eventCenters = _context.EventCenters.Where(ev => ev.Name.Contains(searchString)).ToArray();
+            var conferences = _context.Conferences.Where(c => c.Name.Contains(searchString)).ToArray();
+            var events = _context.Events.Where(e => e.Name.Contains(searchString)).ToArray();
+            var rooms = _context.Rooms.Where(r => r.Name.Contains(searchString)).ToArray();
             var user = await _userManager.FindByNameAsync(searchString);
 
             var searchResults = new SearchResults();
@@ -60,6 +61,8 @@ namespace ConventionManager.Controllers
             {
                 searchResults.User = user;
             }
+
+            ViewData["searchInput"] = searchString;
 
             return View(searchResults);
         }
---
+++ ConventionManager/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ConventionManager.Data;
+using ConventionManager.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace ConventionManager.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> ShowProfile(string userName)
+        {
+            ApplicationUser user;
+            if (userName == "Me")
+            {
+                user = await _userManager.GetUserAsync(HttpContext.User);
+            }
+            else
+            {
+                user = await _userManager.FindByNameAsync(userName);
+            }
+
+            var thisUserName = _userManager.GetUserName(HttpContext.User);
+
+            ViewData["thisUserName"] = thisUserName;
+
+            return View(user);
+        }
+    }
+}
