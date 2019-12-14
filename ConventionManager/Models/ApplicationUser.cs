4d4831a7b16de2ba235cf22064be6a333745d6ab
dasolari <dasolari@uc.cl>
2019-12-14 19:23:57 -0300

    Finished Exhibitor feedback

--- ConventionManager/Models/ApplicationUser.cs
+++ ConventionManager/Models/ApplicationUser.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace ConventionManager.Models
 {
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser, IStatistics<ExhibitorFeedback, ExhibitorQualityStatistics>
     {
         // UserName and Phone number already in IdentityUser class
         public string FirstName { get; set; }
@@ -19,6 +19,42 @@ namespace ConventionManager.Models
         public string FullName()
         {
             return FirstName + " " + LastName;
+        }
+
+        public ExhibitorQualityStatistics GetRating(List<ExhibitorFeedback> feedbacks)
+        {
+            int totalPoints = 0;
+            int overall = 0;
+            int organization = 0;
+            int attention = 0;
+            int roomQuality = 0;
+            int duration = 0;
+            int totalFeedbacks = feedbacks.Count;
+            foreach (var feedback in feedbacks)
+            {
+                overall += feedback.Overall;
+                organization += feedback.Preparation;
+                attention += feedback.Attitude;
+                roomQuality += feedback.Voice;
+                duration += feedback.Connection;
+                totalPoints += feedback.Overall + feedback.Preparation +
+                    feedback.Attitude + feedback.Voice +
+                    feedback.Connection;
+            }
+
+            if (totalFeedbacks == 0)
+            {
+                totalFeedbacks = 1;
+            }
+            var exhibitorQualityStatistics = new ExhibitorQualityStatistics(totalFeedbacks);
+            exhibitorQualityStatistics.SetTotal(totalPoints);
+            exhibitorQualityStatistics.SetOverall(overall);
+            exhibitorQualityStatistics.SetOrganizationOrPreparation(organization);
+            exhibitorQualityStatistics.SetAttentionOrAttitude(attention);
+            exhibitorQualityStatistics.SetRoomQualityOrVoice(roomQuality);
+            exhibitorQualityStatistics.SetDuartionOrConnection(duration);
+
+            return exhibitorQualityStatistics;
         }
     }
 }
